30dab646f4021e82b77a64287e0290f088175632
Soner Çelik <snrclk94@outlook.com>
2021-07-04 01:16:47 +0300

    .

---
+++ Homework1/src/Course.java
@@ -0,0 +1,6 @@
+
+public class Course {
+	int Id;
+	String CourseName;
+	String 
+}
---
+++ Homework1/src/Student.java
@@ -0,0 +1,8 @@
+
+public class Student {
+	int Id;
+	String FirstName;
+	String LastName;
+	String Email;
+	String Password;
+}
